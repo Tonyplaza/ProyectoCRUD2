a3ba6ed2cdfa0a214f0004badbe0d065ffff3d8f
Tonyplaza <plazatony3@gmail.com>
2020-01-09 11:02:18 -0500

    Menu sin terminar con actualizar

---
+++ ProyectoCRUD2/frmActualizar.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoCRUD2
+{
+    public partial class frmActualizar : Form
+    {
+        public frmActualizar()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmActualizar_Load(object sender, EventArgs e)
+        {
+            DataTable dt = Academico.EstudianteDAO.getNombresCompletos();
+            this.cmbMatricula.DataSource = dt;
+            this.cmbMatricula.DisplayMember = "Estudiante";
+            this.cmbMatricula.ValueMember = "Matricula";
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (buscar()==false)
+            {
+                MessageBox.Show("No exite el registro...");
+            }
+        }
+        private bool buscar ()
+        {
+            bool  encontrado = false;
+            DataTable dt = Academico.EstudianteDAO.getDatos(this.cmbMatricula.SelectedValue.ToString());
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow fila in dt.Rows)
+                {
+                    encontrado = true;
+                    this.txtApellidos.Text = fila["apellidos"].ToString();
+                    this.txtNombres.Text = fila["nombres"].ToString();
+                    this.cmbGenero.Text = fila["genero"].ToString();
+                    this.txtCorreo.Text = fila["email"].ToString();
+                    this.txtfechaNacimiento.Text = fila["fechaNacimiento"].ToString();
+                    break;
+                }
+            }
+            return encontrado;
+        }
+
+        private void cmbMatricula_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.cmbMatricula.SelectedIndex >=0 )
+            {
+                buscar();
+            }
+        }
+    }
+}
---
+++ ProyectoCRUD2/frmMenu.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoCRUD2
+{
+    public partial class frmMenu : Form
+    {
+        private int childFormNumber = 0;
+
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowNewForm(object sender, EventArgs e)
+        {
+            //llamada al form para agregar nuevos registros
+            Form1 frm1 = new Form1();
+            frm1.MdiParent = this;
+            frm1.Show();
+        }
+
+        private void OpenFile(object sender, EventArgs e)
+        {
+            //abrir el formulario de busqueda
+            frmBuscar frm1 = new frmBuscar();
+            frm1.MdiParent = this;
+            frm1.Show();
+        }
+
+        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //formulario modificar
+            frmActualizar frm1 = new frmActualizar();
+            frm1.MdiParent = this;
+            frm1.Show();
+        }
+
+        private void ExitToolsStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ToolBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStrip.Visible = toolBarToolStripMenuItem.Checked;
+        }
+
+        private void StatusBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            statusStrip.Visible = statusBarToolStripMenuItem.Checked;
+        }
+
+        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+
+        private void CloseAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Form childForm in MdiChildren)
+            {
+                childForm.Close();
+            }
+        }
+
+        private void fileMenu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmBorrar frm1 = new frmBorrar();
+            frm1.MdiParent = this;
+            frm1.Show();
+        }
+    }
+}

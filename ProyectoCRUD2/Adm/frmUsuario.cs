2ff3e2c85479101397cc7d60367b074047fb7a8d
Tonyplaza <plazatony3@gmail.com>
2020-02-04 16:55:07 -0500

    Proyecto terminado Delete en DataGridView y Actualizar

--- ProyectoCRUD2/Adm/frmUsuario.cs
+++ ProyectoCRUD2/Adm/frmUsuario.cs
@@ -42,23 +42,49 @@ namespace ProyectoCRUD2.Adm
                 this.txtClave.Focus();
                 return;
             }
-            int x = 0;
-            Academico.Usuarios usuarios = new Academico.Usuarios();
-            usuarios.nombreCompleto = this.txtNombreCompleto.Text;
-            usuarios.login = this.txtLogin.Text;
-            usuarios.clave = this.txtClave.Text;
-            usuarios.tipoUsuario = this.cmbTipoUsuario.Text;
-            x = Academico.UsuariosDAO.guardar(usuarios);
-            if (x>0)
+            //Pregunto si el usuario existe en la base de datos 
+            int id = int.Parse(this.txtId.Text.ToString());
+            DataTable dt = Academico.UsuariosDAO.getDatos(id);
+            if (dt.Rows.Count > 0)
             {
+                int x = 0;
+                Academico.Usuarios usuarios = new Academico.Usuarios();
+                usuarios.idLogin = id;
+                usuarios.nombreCompleto = this.txtNombreCompleto.Text;
+                usuarios.login = this.txtLogin.Text;
+                usuarios.clave = this.txtClave.Text;
+                usuarios.tipoUsuario = this.cmbTipoUsuario.Text;
+                try
+                {
+                    x = Academico.UsuariosDAO.actualizar(usuarios);
+                    MessageBox.Show("Filas actualizadas: " + x.ToString());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
                 cargarGridUsuario();
-                MessageBox.Show("Usuario guardado con exito");
             }
             else
             {
-                MessageBox.Show("No se pudo agregar el usario");
+                int x = 0;
+                Academico.Usuarios usuarios = new Academico.Usuarios();
+                usuarios.nombreCompleto = this.txtNombreCompleto.Text;
+                usuarios.login = this.txtLogin.Text;
+                usuarios.clave = this.txtClave.Text;
+                usuarios.tipoUsuario = this.cmbTipoUsuario.Text;
+                x = Academico.UsuariosDAO.guardar(usuarios);
+                if (x > 0)
+                {
+                    cargarGridUsuario();
+                    MessageBox.Show("Usuario guardado con exito");
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo agregar el usario");
+                }
             }
-            
             
         }
         private void cargarGridUsuario()
@@ -81,7 +107,10 @@ namespace ProyectoCRUD2.Adm
             cargarGridUsuario();
             DataGridViewButtonColumn btnSelec = new DataGridViewButtonColumn();
             btnSelec.Name = "Select";
+            DataGridViewButtonColumn btnDelete = new DataGridViewButtonColumn();
+            btnDelete.Name = "Delete";
             dgUsuarios.Columns.Add(btnSelec);
+            dgUsuarios.Columns.Add(btnDelete);
         }
         private void encerar()
         {
@@ -112,17 +141,40 @@ namespace ProyectoCRUD2.Adm
 
                 e.Handled = true;
             }
+            if (e.ColumnIndex >= 0 && this.dgUsuarios.Columns[e.ColumnIndex].Name == "Delete" && e.RowIndex >= 0)
+            {
+                e.Paint(e.CellBounds, DataGridViewPaintParts.All);
+
+                DataGridViewButtonCell celBoton = this.dgUsuarios.Rows[e.RowIndex].Cells["Delete"] as DataGridViewButtonCell;
+                Icon icoAtomico = new Icon(Environment.CurrentDirectory + @"\\delete.ico");/////Recuerden colocar su icono en la carpeta debug de su proyecto
+                e.Graphics.DrawIcon(icoAtomico, e.CellBounds.Left + 3, e.CellBounds.Top + 3);
+
+                this.dgUsuarios.Rows[e.RowIndex].Height = icoAtomico.Height + 8;
+                this.dgUsuarios.Columns[e.ColumnIndex].Width = icoAtomico.Width + 8;
+
+                e.Handled = true;
+            }
         }
 
         private void dgUsuarios_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             if (this.dgUsuarios.Columns[e.ColumnIndex].Name == "Select")
             {
-                txtId.Text = dgUsuarios.CurrentRow.Cells[1].Value.ToString();
-                txtNombreCompleto.Text = dgUsuarios.CurrentRow.Cells[2].Value.ToString();
-                txtLogin.Text = dgUsuarios.CurrentRow.Cells[3].Value.ToString();
-                txtClave.Text = dgUsuarios.CurrentRow.Cells[4].Value.ToString();
-                cmbTipoUsuario.Text = dgUsuarios.CurrentRow.Cells[5].Value.ToString();
+                txtId.Text = dgUsuarios.CurrentRow.Cells[2].Value.ToString();
+                txtNombreCompleto.Text = dgUsuarios.CurrentRow.Cells[3].Value.ToString();
+                txtLogin.Text = dgUsuarios.CurrentRow.Cells[4].Value.ToString();
+                txtClave.Text = dgUsuarios.CurrentRow.Cells[5].Value.ToString();
+                cmbTipoUsuario.Text = dgUsuarios.CurrentRow.Cells[6].Value.ToString();
+            }
+            if (this.dgUsuarios.Columns[e.ColumnIndex].Name == "Delete")
+            {
+                if (MessageBox.Show("¿Estas seguro de eliminar registro de este estudiante?"
+                , "eliminar registro", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    int x = Academico.UsuariosDAO.delete(dgUsuarios.CurrentRow.Cells[2].Value.ToString());
+                    encerar();
+                    cargarGridUsuario();
+                }
             }
         }
     }
--- ProyectoCRUD2/Clases/UsuariosDAO.cs
+++ ProyectoCRUD2/Clases/UsuariosDAO.cs
@@ -11,7 +11,7 @@ namespace Academico
 {
     public static class UsuariosDAO
     {
-        private static string cadenaConexion = @"server=L-PCT-104\SQLEXPRESS2016; database= TI2019; user id=sa; password=Lab123456";
+        private static string cadenaConexion = @"server=A-SIS-050\SQLEXPRESS2016; database= TI2019; user id=sa; password=Lab123456";
       
         public static bool validaUsuario(String usuario, string clave)
         {
@@ -65,6 +65,58 @@ namespace Academico
             ad.Fill(dt);
 
             return dt;
+        }
+        public static DataTable getDatos(int idLogin)
+        {
+            SqlConnection conn = new SqlConnection(cadenaConexion);
+            string sql = "select idLogin,nombreCompleto,login,clave," +
+                 "tipoUsuario from usuarios where idLogin=@idLogin order by nombreCompleto";
+
+            SqlDataAdapter ad = new SqlDataAdapter(sql, conn);
+            ad.SelectCommand.Parameters.AddWithValue("@idLogin", idLogin);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+
+            return dt;
+        }
+        public static int delete(string idLogin)
+        {
+            SqlConnection conn = new SqlConnection(cadenaConexion);
+
+            string sql = "delete from usuarios " +
+                "where idLogin=@idLogin";
+
+            //definimos un comando 
+            SqlCommand comando = new SqlCommand(sql, conn);
+            //vonfiguramos los parametros
+
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.Parameters.AddWithValue("@idLogin", idLogin);
+            conn.Open();
+            int x = comando.ExecuteNonQuery(); //ejecutamos el comando
+            conn.Close();
+            return x;
+        }
+        public static int actualizar(Usuarios usuarios)
+        {
+
+            //definimos un objeo conexion 
+            SqlConnection conn = new SqlConnection(cadenaConexion);
+            string sql = "UPDATE usuarios SET nombreCompleto=@nombreCompleto,login=@login,clave=@clave," +
+                "tipoUsuario=@tipoUsuario WHERE idLogin=@idLogin ";
+            //definimos un comando 
+            SqlCommand comando = new SqlCommand(sql, conn);
+            //vonfiguramos los parametros
+            conn.Open();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.Parameters.AddWithValue("@idLogin", usuarios.idLogin);
+            comando.Parameters.AddWithValue("@nombreCompleto", usuarios.nombreCompleto);
+            comando.Parameters.AddWithValue("@login", usuarios.login);
+            comando.Parameters.AddWithValue("@clave", usuarios.clave);
+            comando.Parameters.AddWithValue("@tipoUsuario", usuarios.tipoUsuario);
+            int x = comando.ExecuteNonQuery(); //ejecutamos el comando
+            conn.Close();
+            return x;
         }
     }
 }
